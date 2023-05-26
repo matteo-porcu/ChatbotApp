78cee2eeade830c5928d20e4cc691f296bf65472
Matteo Porcu <matteo.porcu@overapp.it>
2023-05-26 11:16:21 +0200

    Initial Commit

---
+++ ChatbotProject/ChatbotProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatbotProjectApp.swift
+//  ChatbotProject
+//
+//  Created by matteo on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatbotProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
